6330e16f9ce60439116c2b1e4acb555c1d1d6340
Asmita Thapa <asmitathapa429@gmail.com>
2024-04-22 18:13:13 +0545

    import example

---
+++ import/math.js
@@ -0,0 +1,3 @@
+export const sum = (a, b) => a + b;
+export const mul = (a, b) => a * b;
+export const g = 9.8;
---
+++ import/script.js
@@ -0,0 +1,4 @@
+import { sum, g } from "./math.js";
+
+console.log(sum(9, 4));
+console.log("value of G = ", g);
