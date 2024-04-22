b37e63b36f556003904c86ffc5ff6df4dc047cbc
Asmita Thapa <asmitathapa429@gmail.com>
2024-04-22 18:20:59 +0545

    random word generator package added

--- import/script.js
+++ import/script.js
@@ -1,4 +1,11 @@
-import { sum, g } from "./math.js";
+// import { sum, g } from "./math.js";
+import { generate } from "random-words";
 
-console.log(sum(9, 4));
-console.log("value of G = ", g);
+// console.log(sum(9, 4));
+// console.log("value of G = ", g);
+
+// generated random words.
+console.log(generate());
+
+// do not push node_modules in git, it can be simply
+// installed again by navigating dependencies on package.json file
